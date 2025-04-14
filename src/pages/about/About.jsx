20478221dfc4baa34e948fc64c680e044c61180c
Mizi 2.0 <mrshoayeb75@gmail.com>
2025-04-14 09:29:47 +0600

    third commit

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import aboutBgImg from "../../assets/about-background.jpg"
+import Expricences from '../home/Expricences.jsx'
+
 const About = () => {
   return (
-    <div>About</div>
+    <section> 
+     {/* banner */}
+     <div
+        className="w-full h-[400px]  bg-no-repeat bg-cover  flex items-center justify-center text-white"
+        style={{ backgroundImage: `url(${aboutBgImg})` }}
+      >
+        <div className=''>
+        <h1 className="text-5xl font-bold">About Us</h1>
+        </div>
+      </div>
+      <Expricences/>
+    </section>
   )
 }
 
--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import contactBgImg from "../../assets/contact-background.jpg"
+import Materials from '../home/Materials'
+import Testimonials from '../home/Testimonial'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section> 
+    {/* banner */}
+    <div
+       className="w-full h-[400px]  bg-no-repeat bg-cover bg-center   flex items-center justify-center text-white"
+       style={{ backgroundImage: `url(${contactBgImg})` }}
+     >
+       <div className=''>
+       <h1 className="text-5xl font-bold">Contact</h1>
+       </div>
+     </div>
+     <Materials/>
+     <Testimonials/>
+   </section>
   )
 }
 
